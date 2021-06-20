1071799c0dbd93c669252e380769780f4a768e9a
Nicolas Cardia <nicolascardia@gmail.com>
2021-06-19 21:25:44 -0300

    adicionando README.md

---
+++ README.md
@@ -0,0 +1,90 @@
+<p align="center">
+<img  style="border-radius: 8px; width:50%;" src=""/>
+</p>
+
+# Índice
+
+- [Desafio](#Desafio)
+
+- [Premissas](#Premissas)
+
+- [Tecnologia escolhida para realizar o projeto](#Tecnologia-escolhida-para-realizar-o-projeto)
+
+- [Como rodar o projeto](#como-rodar-o-projeto)
+
+- [Informações Técnicas](#Informações-Técnicas)
+- 
+# Desafio
+
+Imagine uma lista de compras. Ela possui:
+
+<ul>
+
+<li>Itens
+
+<li>Quantidade de cada item
+
+<li>Preço por unidade/peso/pacote de cada item
+
+**Desenvolva uma função (ou método) que irá receber uma lista de compras (como a detalhada acima) e uma lista de e-mails. Aqui, cada e-mail representa uma pessoa**.
+</ul>
+
+**A função deve**:
+
+- Calcular a soma dos valores, ou seja, multiplicar o preço de cada item por sua quantidade e somar todos os itens
+
+- Dividir o valor de forma igual entre a quantidade de e-mails
+
+- Retornar um mapa/dicionário onde a chave será o e-mail e o valor será quanto ele deve pagar nessa conta.
+
+# Premissas
+1º É **importante que não falte nenhum centavo!** Então caso ocorra uma divisão que o resultado é uma dizima infinita, o "resto" do valor deve ser distribuido entre os e-mails, até que o resto fique igual a **0** 
+>**exemplo:**
+> valorTotalProdutos = 3
+> e-mails = 2
+> valorTotalProdutos(3) ÷ e-mails(2) = **1** (valorOriginal = 1.5)
+> resto = 1
+> **Resultado:**
+> 1º e-mail deve pagar **2**
+> 2º e-mail deve pagar **1**
+
+2º **NÃO trabalhe com valores decimais**. Ou seja, ponto flutuante ou float. Use **inteiros** para representar os valores!
+
+3º A quantidade de e-mails deve ser **menor** que o valor total dos produtos, para evitar que gere números menores que **1** . 
+
+# Tecnologia escolhida para realizar o Teste Técnico
+  ![JavaScript](https://img.shields.io/badge/-JavaScript-333333?style=flat&logo=javascript)
+> por ser a linguagem que eu mais tenho conhecimento atualmente.
+
+# Como rodar o projeto
+
+## Primeiro Passo
+De um git clone no projeto e abra o arquivo **index.html** no seu navegador
+
+## Segundo Passo
+
+Você recebera uma lista randomica de emails, uma lista randomica de produtos e a soma do valor total dos mesmos e o resto (caso o resultado da divisão seja um valor decimal) que é o valor que vamos distribuir entre os email-s
+
+## Terceiro Passo
+Clicar no botão "Gerar lista de pagamento" para poder ver quanto cada e-mail deve pagar 
+-> Caso queira repetir o processo com outros números basta clicar no botão "gerar outra lista".
+
+# Informações Técnicas
+**1º Crio duas classes, item e pagamento**
+-  a class item é para criar um objeto "item" com todos os parâmentros nescessarios 
+- a class pagamento é para gerar o objeto de pagamento que recebe o email e o valor a ser pago
+
+**2º crio a minhas variáveis** 
+> para serem usadas nas funções e os arrays vazio serem preenchidos com o metodo .push() nas funções gerarItens(), gerarEmails() e criarListaPagamento 
+
+**2º crio as minhas funções**
+que criam os itens, e-mails e listaDePagamentos e realizam as lógicas matemáticas.
+
+>**Lógica matemática:** 
+quantidade * preco = valorTotalProduto
+valorTotalProdutos + valorTotalProduto = valorTotalProdutos
+valorTotalProdutos / qtdEmail = valorPorPessoa
+qtdEmail % valorTotalProdutos = restoDaDivisão
+
+**3º Printo os resultados na Tela**
+>Printo na tela usando O Modelo de Objeto de Documento (**DOM**) 
--- src/js/randomList.js
+++ src/js/randomList.js
@@ -1,9 +1,8 @@
 class item{
-    constructor (item, quantidade, preco, unidadeMedida, descricaoUnidadeMedida) {
+    constructor (item, quantidade, preco, descricaoUnidadeMedida) {
         this.item = item;
         this.quantidade = quantidade;
         this.preco = preco;
-        this.unidadeMedida = unidadeMedida;
         this.descricaoUnidadeMedida = descricaoUnidadeMedida;
     }
 }
@@ -11,19 +10,20 @@ class item{
 class pagamento {
     constructor(email, valor) {
         this.email = email;
-        this.valor = valor;
+        this.valor_a_ser_pago = valor;
     }
 }
 
+
 var valorTotalProdutos;
 var qtdEmail;
-var resto;
+var restoDivisao;
 var valorPorPessoa;
 
 
 var listaItens = [];
 var listaEmail = [];
-var listaCompras = [];
+var listaPagamento = [];
 
 var elemento;
 
@@ -32,7 +32,7 @@ function gerarItens(){
     let listaUnidadeMedida = ['kg', 'unidade', 'pacote'];
     
     for (var i = 0; i < Math.floor(Math.random() * 5) + 1; i++) {
-        listaItens.push(new item (`item ${i}`,Math.floor(Math.random() * 10) + 1, Math.floor(Math.random() * 50 ) + 1, 1, `${elemento = listaUnidadeMedida[Math.floor(Math.random() * listaUnidadeMedida.length)]}`),);     
+        listaItens.push(new item (`item ${i}`,Math.floor(Math.random() * 10) + 1, Math.floor(Math.random() * 50 ) + 1, `${elemento = listaUnidadeMedida[Math.floor(Math.random() * listaUnidadeMedida.length)]}`),);     
     }    
 
 }
@@ -62,21 +62,21 @@ gerarEmails();
 function valorTotalItens(listaItensInterno){
     valorTotalProdutos = 0
     if(listaItensInterno.length > 1){
-        document.getElementById('tela').innerHTML += `<br>A quantidade de itens comprados são ${listaItensInterno.length}<br>`
+        document.getElementById('tela').innerHTML += `<br>A quantidade de produtos comprados são ${listaItensInterno.length}<br>`
     }else{
         document.getElementById('tela').innerHTML += `<br>Apenas ${listaItensInterno.length} item foi comprado<br>`
 
     }
 
     for (let i = 0; i < listaItensInterno.length; i++) {
-        document.getElementById('tela').innerHTML += `O ${listaItensInterno[i].item} tem a quantidade de ${listaItensInterno[i].quantidade} (${listaItensInterno[i].descricaoUnidadeMedida}) e custa ${listaItensInterno[i].preco} centavos <br>`
-        valorTotalProdutos = valorTotalProdutos + (listaItensInterno[i].quantidade * listaItensInterno[i].preco)
+        document.getElementById('tela').innerHTML += `O ${listaItensInterno[i].item} tem a quantidade de ${listaItensInterno[i].quantidade} (${listaItensInterno[i].descricaoUnidadeMedida}) e custa R$ ${listaItensInterno[i].preco} <br>`
+        valorTotalProdutos += (listaItensInterno[i].quantidade * listaItensInterno[i].preco)
     }
 
     if(listaItensInterno.length > 1){
-        document.getElementById('tela').innerHTML += `<br>O valor total dos produtos é ${valorTotalProdutos} centavos <br>`
+        document.getElementById('tela').innerHTML += `<br>O valor total dos produtos é R$ ${valorTotalProdutos} <br>`
     } else {
-        document.getElementById('tela').innerHTML += `<br>O valor total do produto é ${valorTotalProdutos} centavos <br>`
+        document.getElementById('tela').innerHTML += `<br>O valor total do produto é R$ ${valorTotalProdutos} <br>`
     }
 }
 valorTotalItens(listaItens);
@@ -88,34 +88,34 @@ function divideValorPorEmail(listaEmailInterno) {
     valorPorPessoa = 0
 
     valorPorPessoa = Math.floor(valorTotalProdutos / qtdEmail)
-    document.getElementById('tela').innerHTML += `<br>valor que cada pessoa deve pagar é ${valorPorPessoa} <br>`
+    document.getElementById('tela').innerHTML += `<br>valor que cada pessoa deve pagar é R$ ${valorPorPessoa} <br>`
 
 }     
 divideValorPorEmail(listaEmail);
 
 
-function pegarResto(){
-    resto = 0
-    resto = (valorTotalProdutos % qtdEmail) 
-    document.getElementById('tela').innerHTML += `<br>resto do valor a ser distribuido "${resto}" <br><br>`
+function pegarrestoDivisaoDivisao(){
+    restoDivisao = 0
+    restoDivisao = (valorTotalProdutos % qtdEmail) 
+    document.getElementById('tela').innerHTML += `<br>Valor a ser distribuido ${restoDivisao} <br><br>`
     document.getElementById("tela").innerHTML += `<button class="botao" onclick="printarListaPagamentos()">Gerar lista de pagamentos</button><br>`;
   
 }
-pegarResto();
+pegarrestoDivisaoDivisao();
 
 
 function  criaListaPagamentos(listaEmailInterno ){
-    let distribuicaoValorResto = 0
+    let distribuicaoValorRestoDivisao = 0
 
     if(valorTotalProdutos > qtdEmail) {
         for (var i = 0; i < listaEmailInterno.length; i++) {
-            if (resto > 0) {
-                distribuicaoValorResto = 1 
-                resto -= distribuicaoValorResto
+            if (restoDivisao > 0) {
+                distribuicaoValorRestoDivisao = 1 
+                restoDivisao -= distribuicaoValorRestoDivisao
             }
             
-            listaCompras.push(new pagamento (listaEmailInterno[i], valorPorPessoa + distribuicaoValorResto + `<br>` ));
-            distribuicaoValorResto = 0
+            listaPagamento.push(new pagamento (listaEmailInterno[i], valorPorPessoa + distribuicaoValorRestoDivisao + `<br>` ));
+            distribuicaoValorRestoDivisao = 0
             
         }
     } else {
@@ -128,21 +128,13 @@ criaListaPagamentos(listaEmail);
 
 function printarListaPagamentos(){
 
-    str = JSON.stringify(listaCompras);
+    str = JSON.stringify(listaPagamento);
     document.getElementById('tela').innerHTML = str ;        
     document.getElementById("tela").innerHTML += `<br><br><button class="botao" onclick="window.location.reload()">Gerar outra lista</button>`;
 
 }
 
 
-console.log()
-/*function BtnStart(){
-    document.getElementById("tela").innerHTML = `<button class="botao" onclick="gerarEmails();gerarItens();valorTotalItens(listaItens);divideValorPorEmail(listaEmail);pegarResto();printarListaPagamentos()">Gerar lista Aleatória</button>
-    </button>`;
-}
-BtnStart();*/
-
-    //document.getElementById("tela").innerHTML += listaCompras.join();
 
 
 
